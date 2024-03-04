f577b72ddb6b3625de691bdfae53aa9d4a0144fd
Eliezer <eliezerTerrero12@gmail.com>
2024-03-04 18:45:05 -0400

    agregando el contexto

--- Eliezer_Terrero_P2_AP1.API/DAL/Contexto.cs
+++ Eliezer_Terrero_P2_AP1.API/DAL/Contexto.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Shared;
+using Shared.Models;
 
 namespace Eliezer_Terrero_P2_AP1.API.DAL
 {
-    public class Contexto: DbContext
-    {
-       
-    }
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions options) : base(options)
+		{
+		}
+		public DbSet<Vehiculos> Vehiculos { get; set;}
+		public DbSet<Accesorios> Accesorios { get; set; }
+		public DbSet<VehiculosDetalle> VehiculosDetalle { get; set; }
+	}
 }
