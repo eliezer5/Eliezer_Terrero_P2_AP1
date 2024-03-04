9d5727412e56068a2058e30f42b19c7a34fb1f40
Eliezer <eliezerTerrero12@gmail.com>
2024-03-04 18:44:02 -0400

    Creando la clase Vehiculos

---
+++ Shared/Models/Vehiculos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Shared.Models
+{
+    public class Vehiculos
+    {
+        [Key]
+        public int VehiculoId { get; set; }
+        [Required(ErrorMessage = "El Campo es requerido")]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "El Campo es requerido")]
+        public string Descripcion { get; set; }
+
+
+		[Required(ErrorMessage = "El Campo es requerido")]
+		public decimal Costo { get; set; }
+
+		[Required(ErrorMessage = "El Campo es requerido")]
+        public decimal Gasto { get; set; }
+
+        public ICollection<VehiculosDetalle?> VehiculosDetalles { get; set; } = new List<VehiculosDetalle?>();
+    }
+}
