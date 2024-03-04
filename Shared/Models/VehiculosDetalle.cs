3e6ab8123a044195fe8a9065485ef137749c2b83
Eliezer <eliezerTerrero12@gmail.com>
2024-03-04 18:44:36 -0400

    Creando la clase VehiculosDetalle

---
+++ Shared/Models/VehiculosDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class VehiculosDetalle
+	{
+		[Key]
+		public int Id { get; set; }
+		[ForeignKey("Vehiculos")]
+		public int VehiculoId { get; set; }
+		[ForeignKey("Accesorios")]
+		public int AccesorioId { get; set; }
+		[Required(ErrorMessage ="El campo es requerido")]
+		public decimal Valor {  get; set; }
+		
+	}
+}
