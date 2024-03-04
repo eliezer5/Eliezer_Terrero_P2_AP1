0288554d62ee0ae2b00adb6c9362d0f18c93e100
Eliezer <eliezerTerrero12@gmail.com>
2024-03-04 18:44:16 -0400

    Creando la clase Accesorio

---
+++ Shared/Models/Accesorios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Accesorios
+	{
+		[Key]	
+		public int AccesoriosId {  get; set; }
+
+		[Required]
+		public string Descripcion { get; set; }
+	}
+
+}
