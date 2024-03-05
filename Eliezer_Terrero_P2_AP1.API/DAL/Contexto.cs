4dd209f6ec97db807be2d5339fe669dfea3aa69d
Eliezer <eliezerTerrero12@gmail.com>
2024-03-04 20:49:51 -0400

    Creando onModelCreate

--- Eliezer_Terrero_P2_AP1.API/DAL/Contexto.cs
+++ Eliezer_Terrero_P2_AP1.API/DAL/Contexto.cs
@@ -12,5 +12,19 @@ namespace Eliezer_Terrero_P2_AP1.API.DAL
 		public DbSet<Vehiculos> Vehiculos { get; set;}
 		public DbSet<Accesorios> Accesorios { get; set; }
 		public DbSet<VehiculosDetalle> VehiculosDetalle { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Accesorios>().HasData(new List<Accesorios>()
+			{
+				new Accesorios() {AccesoriosId=1, Descripcion ="Camara Trasera"},
+				new Accesorios() {AccesoriosId=2, Descripcion ="Pantalla interior"},
+				new Accesorios() {AccesoriosId=3, Descripcion ="Interior en Piel"},
+				new Accesorios() {AccesoriosId=4, Descripcion ="Sun Roof"},
+				new Accesorios() {AccesoriosId=5, Descripcion ="Aros de Lujo"}
+
+			});
+		}
 	}
 }
